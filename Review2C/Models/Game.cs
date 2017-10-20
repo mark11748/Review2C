fcba2f0fa24881316fe9a81013e8ce8427a0a3a7
Mark W <markwood117@gmail.com>
2017-10-20 09:37:09 -0700

    necessary properties ,getters and setters declared for use with test methods

--- Review2C/Models/Game.cs
+++ Review2C/Models/Game.cs
@@ -4,8 +4,29 @@ using System.Linq;
 
 namespace Review2C.Models
 {
-  public class Obj
+  public class RepeatCounter
   {
+    private string _primary; //this is the string being searched
+    private string _target;  //this is the string being looked for
+    private int    _wordCount;
 
+    public RepeatCounter(string p, string t)
+    {
+      
+    }
+
+    public string  GetPrimary(){return _primary;}
+    public void    SetPrimary(string p){_primary = p}
+
+    public string   GetTarget(){return _target;}
+    public void     SetTarget(string t){_target = t;}
+
+    public int   GetWordCount(){return _wordCount;}
+    public void  SetWordCount(){}
+
+    public CountRepeats()
+    {
+
+    }
   }
 }
